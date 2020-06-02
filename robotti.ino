5acad329e9f98000cb5b22cf02ff1b0a4c6afa0e
jusola <juulain@gmail.com>
2020-06-02 20:58:52 +0300

    Initial

--- robotti.ino
+++ robotti.ino
@@ -28,6 +28,19 @@ MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 MeLineFollower lineFinder(PORT_9);
 
+const float startSpeed = 0.5;
+
+float turnDirection = 0; // 0 = ahead, 1 = right, -1 = left
+float speed = startSpeed; //0 to 1
+float forward = 1;
+
+int timeOutsideLine = 0;
+int timeOutsideLineThreshold = 25; // time before backing up
+int timeOutsideLineThresholdBack = 35;
+
+const int motorPWM1 = -100;
+const int motorPWM2 = 100;
+
 void isr_process_encoder1(void)
 {
   if(digitalRead(Encoder_1.getPortB()) == 0)
@@ -68,23 +81,73 @@ void setup()
 
 void loop()
 {
-  Encoder_1.setMotorPwm(-100);
-  Encoder_2.setMotorPwm(100);
   Encoder_1.updateSpeed();
   Encoder_2.updateSpeed();
-  Serial.print("Speed 1:");
-  Serial.print(Encoder_1.getCurrentSpeed());
-  Serial.print(" ,Speed 2:");
-  Serial.println(Encoder_2.getCurrentSpeed());
   int sensorState = lineFinder.readSensors();
+  
   switch(sensorState)
   {
-    case S1_IN_S2_IN: Serial.println("Sensor 1 and 2 are inside of black line"); break;
-    case S1_IN_S2_OUT: Serial.println("Sensor 2 is outside of black line"); break;
-    case S1_OUT_S2_IN: Serial.println("Sensor 1 is outside of black line"); break;
-    case S1_OUT_S2_OUT: Serial.println("Sensor 1 and 2 are outside of black line"); break;
-    default: break;
+    case S1_IN_S2_IN: // both out -- forward a bit, if no line, back and turn
+      if(timeOutsideLine == 0){
+        speed = 2.5*startSpeed;
+        turnDirection = 0;
+        forward = 1;
+      }
+      if(timeOutsideLine > timeOutsideLineThreshold){
+        speed = 2.5*startSpeed;
+        turnDirection = 0;
+        forward = -1;
+      }
+      if(timeOutsideLine > timeOutsideLineThreshold+timeOutsideLineThresholdBack){
+        speed = startSpeed / 2;
+        forward = 1;
+        turnDirection = 1;
+      }
+      timeOutsideLine++;
+      break;
+    case S1_IN_S2_OUT: // left out - turn right
+      timeOutsideLine = 0;
+      forward = 1;
+      speed = startSpeed;
+      turnDirection = 1;
+      break;
+    case S1_OUT_S2_IN: // right out - turn left
+      timeOutsideLine = 0;
+      forward = 1;
+      speed = startSpeed;
+      turnDirection = -1;
+      break;
+    case S1_OUT_S2_OUT: // both in -- ahead
+      timeOutsideLine = 0;
+      forward = 1;
+      speed = startSpeed;
+      turnDirection = 0;
+      break;
+    default:
+      forward = 1;
+      speed = startSpeed;
+      turnDirection = 0;
+      break;
   }
-  delay(100);
+  drive();
 }
 
+void drive()
+{
+  float left = speedL(turnDirection, speed*forward);
+  float right = speedR(turnDirection, speed*forward);
+  setSpeeds(left, right);
+}
+
+void setSpeeds(float left, float right){
+  Encoder_1.setMotorPwm(left*motorPWM1);
+  Encoder_2.setMotorPwm(right*motorPWM2);
+}
+
+float speedL(float turn, float speed){
+  return speed*((-2)*turn+1);
+}
+
+float speedR(float turn, float speed){
+  return speed*(2*turn+1);
+}
